9411f7c67db555acb1a674674e031546f2e3a6fd
drmostafa710 <drmostafa710@gmail.com>
2025-05-11 15:55:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-// Firebase Messaging Service Worker
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+// // Firebase Messaging Service Worker
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
 
-import {
-  getMessaging,
-  onBackgroundMessage,
-} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-messaging.js";
+// import {
+//   getMessaging,
+//   onBackgroundMessage,
+// } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-messaging.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBqzGlq5Qukfk1WKzRCl9L8ndH2FnAct7k",
-  authDomain: "qzapp-3db60.firebaseapp.com",
-  projectId: "qzapp-3db60",
-  storageBucket: "qzapp-3db60.appspot.com",
-  messagingSenderId: "512223543409",
-  appId: "1:512223543409:web:8167a582ef6f5a37661bd0",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBqzGlq5Qukfk1WKzRCl9L8ndH2FnAct7k",
+//   authDomain: "qzapp-3db60.firebaseapp.com",
+//   projectId: "qzapp-3db60",
+//   storageBucket: "qzapp-3db60.appspot.com",
+//   messagingSenderId: "512223543409",
+//   appId: "1:512223543409:web:8167a582ef6f5a37661bd0",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const messaging = getMessaging(app);
 
 // Firebase Messaging Service Worker
 self.addEventListener("push", (event) => {
@@ -40,21 +40,21 @@ self.addEventListener("notificationclick", (event) => {
   event.waitUntil(clients.openWindow(event.notification.data.url));
 });
 
-onBackgroundMessage(messaging, (payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  const notificationTitle = payload.notification?.title || "Default Title";
-  const notificationOptions = {
-    body: payload.notification?.body || "Default Body",
-    icon: payload.notification?.image || "/default-icon.png",
-    data: { url: payload.data?.link || "https://example.com" },
-    tag: "my-tag",
-  };
+// onBackgroundMessage(messaging, (payload) => {
+//   console.log(
+//     "[firebase-messaging-sw.js] Received background message ",
+//     payload
+//   );
+//   const notificationTitle = payload.notification?.title || "Default Title";
+//   const notificationOptions = {
+//     body: payload.notification?.body || "Default Body",
+//     icon: payload.notification?.image || "/default-icon.png",
+//     data: { url: payload.data?.link || "https://example.com" },
+//     tag: "my-tag",
+//   };
 
-  return self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
-});
+//   return self.registration.showNotification(
+//     notificationTitle,
+//     notificationOptions
+//   );
+// });
