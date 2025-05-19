90faed97a09bb905e9b5d4554d5788a4be08e93b
drmostafa710 <drmostafa710@gmail.com>
2025-05-19 20:46:19 +0300

    new sw

---
+++ sw-v1.js
@@ -0,0 +1,52 @@
+// Firebase Messaging Service Worker
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
+
+import {
+  getMessaging,
+  onBackgroundMessage,
+} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-messaging-sw.js";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqzGlq5Qukfk1WKzRCl9L8ndH2FnAct7k",
+  authDomain: "qzapp-3db60.firebaseapp.com",
+  projectId: "qzapp-3db60",
+  storageBucket: "qzapp-3db60.appspot.com",
+  messagingSenderId: "512223543409",
+  appId: "1:512223543409:web:8167a582ef6f5a37661bd0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+// // Background message handler (via FCM when app is in background)
+onBackgroundMessage(messaging, (payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message:",
+    payload.fcmOptions.link
+  );
+
+  const notificationTitle = payload.notification?.title || "New Quiz Launched";
+  const notificationOptions = {
+    body: payload.notification?.body || "Click To Open",
+    icon: payload.notification?.image || "/default-icon.png",
+    data: {
+      url: payload.fcmOptions.link,
+    },
+    tag: "fcm-background",
+  };
+
+  self.addEventListener("notificationclick", (event) => {
+    const urlToOpen = payload.fcmOptions.link;
+
+    event.waitUntil(clients.openWindow(urlToOpen));
+
+    event.notification.close();
+  });
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
