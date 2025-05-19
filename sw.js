515f5f55c605e07e9bf1b39e7d226ea8aa3ff480
drmostafa710 <drmostafa710@gmail.com>
2025-05-19 13:39:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,3 @@
-// Firebase Messaging Service Worker
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js";
-import {
-  getMessaging,
-  onBackgroundMessage,
-} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-messaging-sw.js";
-
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBqzGlq5Qukfk1WKzRCl9L8ndH2FnAct7k",
-  authDomain: "qzapp-3db60.firebaseapp.com",
-  projectId: "qzapp-3db60",
-  storageBucket: "qzapp-3db60.appspot.com",
-  messagingSenderId: "512223543409",
-  appId: "1:512223543409:web:8167a582ef6f5a37661bd0",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-
 self.addEventListener("push", function(event) {
   const payload = event.data.json();
 
