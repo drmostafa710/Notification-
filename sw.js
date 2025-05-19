275b58977150ce7f3dee5aaaca34859d716d7d82
drmostafa710 <drmostafa710@gmail.com>
2025-05-19 13:05:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,70 +19,28 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-// // Handle push from PHP backend payload
-// self.addEventListener("push", (event) => {
-//   const payload = event.data.json();
-//   console.log("Push event received:", payload.notification.click_action);
+self.addEventListener("push", function(event) {
+  const payload = event.data.json();
 
-//   // Structure per your PHP format
-//   const title = payload.notification?.title || "No Title";
-//   const body = payload.notification?.body || "No Body";
-//   const icon = payload.notification?.image || "/default-icon.png";
-//   const clickUrl = payload.notification.click_action;
+  console.log("[firebase-messaging-sw.js] Received push message:", payload);
 
-//   const options = {
-//     body: body,
-//     icon: icon,
-//     data: {
-//       url: clickUrl,
-//     },
-//   };
-
-//   event.waitUntil(self.registration.showNotification(title, options));
-  
-//   self.addEventListener("notificationclick", (event) => {
-  
-//     console.log(event)
-    
-//     const urlToOpen = clickUrl || "https://google.com";
-//     event.waitUntil(clients.openWindow(urlToOpen));
-    
-//     event.notification.close();
-//   });
-// });
-
-
-// // Background message handler (via FCM when app is in background)
-onBackgroundMessage(messaging, (payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message:",
-    payload.fcmOptions.link
-  );
-
-
-  
-  const notificationTitle = payload.notification?.title || "Default Title";
+  const notificationTitle = payload.notification?.title;
   const notificationOptions = {
-    body: payload.notification?.body ,
-    icon: payload.notification?.image ,
+    body: payload.notification?.body,
+    icon: payload.notification?.image,
     data: {
-      url: payload.fcmOptions.link,
-    }
+      url: payload.fcmOptions?.link || "https://google.com",
+    },
   };
 
-  self.addEventListener("notificationclick", (event) => {
-  
-    console.log(event)
-    
-    const urlToOpen = payload.fcmOptions.link || "https://google.com";
-    event.waitUntil(clients.openWindow(urlToOpen));
-    
-    event.notification.close();
-  });
-  
-  
-  return self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
+  event.waitUntil(
+    self.registration.showNotification(notificationTitle, notificationOptions)
   );
+});
+
+self.addEventListener("notificationclick", function(event) {
+  const url = event.notification.data?.url || "https://google.com";
+
+  event.notification.close();
+  event.waitUntil(clients.openWindow(url));
 });
