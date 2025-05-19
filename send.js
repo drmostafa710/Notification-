c318c083d1e54ce910f9b7bdfe39ccfe952e5724
drmostafa710 <drmostafa710@gmail.com>
2025-05-19 10:24:34 +0300

    Update send.js

--- send.js
+++ send.js
@@ -46,7 +46,7 @@ const messaging = getMessaging(app);
                 tokenElement.textContent = currentToken;
                 document.querySelector(".token").appendChild(tokenElement);
 
-                fetch('http://localhost:8000/index.php?page=home', {
+                fetch('http://localhost:8000/', {
                   method: 'POST',
                   headers: {
                     'Content-Type': 'application/json',
