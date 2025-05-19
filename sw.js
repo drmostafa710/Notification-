8ba7596bf1c21e05117210743c8ebd73aa073623
drmostafa710 <drmostafa710@gmail.com>
2025-05-19 14:12:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,28 @@
 self.addEventListener("push", function(event) {
   const payload = event.data.json();
 
-  console.log("[firebase-messaging-sw.js] Received push message:", payload);
+  console.log("[firebase-messaging-sw.js] Received push message:", payload.fcmOptions?.link);
 
   const notificationTitle = payload.notification?.title;
   const notificationOptions = {
     body: payload.notification?.body,
     icon: payload.notification?.image,
     data: {
-      url: payload.fcmOptions?.link || "https://google.com",
+      url: payload.fcmOptions?.link || "https://quiz-app.infinityfreeapp.com/",
     },
   };
 
   event.waitUntil(
     self.registration.showNotification(notificationTitle, notificationOptions)
   );
+
+  self.addEventListener("notificationclick", function(event) {
+    const url = payload.fcmOptions?.link;
+
+    event.waitUntil(clients.openWindow(url));
+    
+    event.notification.close();
+  });
 });
 
-self.addEventListener("notificationclick", function(event) {
-  const url = event.notification.data?.url || "https://google.com";
 
-  event.notification.close();
-  event.waitUntil(clients.openWindow(url));
-});
